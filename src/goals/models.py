9c8d59e46d79db56e4e34bf636d6d6b26d165b23
Rohan Roy <b4you0870@gmail.com>
2017-03-11 02:24:43 +0530

    Goal and Subgoal model were updated

--- src/goals/models.py
+++ src/goals/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.template.defaultfilters import slugify
+from datetime import datetime
+from django.core.exceptions import ValidationError
 
 User = settings.AUTH_USER_MODEL
 
@@ -10,10 +12,10 @@ class Goal(models.Model):
     """
     Goal objects
     """
-    title = models.CharField(max_length=100, null=False, blank=False)
-    slug = models.SlugField(max_length=150, null=True, blank=True)
+    title = models.CharField(max_length=100, blank=False, null=False)
+    slug = models.SlugField(max_length=150, blank=True, null=True)
     description = models.TextField(null=True, blank=True)
-    user = models.OneToOneField(User)
+    user = models.ForeignKey(User)
     end_date = models.DateField()
     created_at = models.DateTimeField(editable=False, auto_now_add=True)
     updated_at = models.DateTimeField(editable=False, auto_now_add=True)
@@ -27,14 +29,20 @@ class Goal(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
+        self.full_clean()
+        self.validate_end_date()
         super(Goal, self).save(*args, **kwargs)
+
+    def validate_end_date(self):
+        if self.end_date < datetime.now().date():
+            raise ValidationError("End date should not be in past")
 
 
 class Subgoal(models.Model):
-    title = models.CharField(max_length=100, null=True, blank=True)
+    title = models.CharField(max_length=100)
     slug = models.CharField(max_length=150, null=True, blank=True)
-    goal = models.ForeignKey(Goal)
-    end_date = models.DateField()
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
+    end_date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(editable=False, auto_now_add=True)
     updated_at = models.DateTimeField(editable=False, auto_now_add=True)
 
@@ -47,4 +55,13 @@ class Subgoal(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
+        self.full_clean()
+        if not self.end_date:
+            self.end_date = self.goal.end_date
+        else:
+            self.validate_end_date()
         super(Subgoal, self).save(*args, **kwargs)
+
+    def validate_end_date(self):
+        if self.end_date < datetime.now().date():
+            raise ValidationError("End date should not be in past")
