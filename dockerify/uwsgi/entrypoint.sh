e193ba5174e35ef9c3509caf6a05f5e870687ca6
Rohan Roy <b4you0870@gmail.com>
2017-03-17 12:17:09 +0530

    celery config was changed

--- dockerify/uwsgi/entrypoint.sh
+++ dockerify/uwsgi/entrypoint.sh
@@ -17,16 +17,17 @@ rabbitmq-plugins enable rabbitmq_management
 /etc/init.d/rabbitmq-server restart
 /etc/init.d/supervisor restart
 
+echo yes | python src/manage.py migrate
+echo yes | python src/manage.py collectstatic --noinput
+
 supervisorctl update
 supervisorctl status mysanacelery
 supervisorctl status mysanacelerybeat
 
-echo yes | python src/manage.py migrate
-echo yes | python src/manage.py collectstatic --noinput
-
 # Forward app logs to docker log collector
 tail -n0 -F /var/log/app_logs/*.log &
-tail -n0 -F /vat/log/celery/*.log &
+tail -n0 -F /var/log/celery/*.log &
+tail -n0 -F /var/log/supervisor/supervisord.log &
 
 # start uwsgi
 exec uwsgi --emperor dockerify/uwsgi/ --gid www-data
