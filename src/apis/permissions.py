3bad053bd7edb5591470bd3ca5f63b6be64b7bce
Rohan Roy <b4you0870@gmail.com>
2017-03-19 23:09:33 +0530

    typo in permission was corrected

--- src/apis/permissions.py
+++ src/apis/permissions.py
@@ -3,6 +3,6 @@ from rest_framework import permissions
 
 class IsOwnerOrModerator(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.user.is_staff():
+        if request.user.is_staff:
             return True
-        return obj.user == request.userz
+        return obj.user == request.user
