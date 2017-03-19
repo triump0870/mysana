2916cda33c0beae06fa5556ad2f813d430c59fe5
Rohan Roy <b4you0870@gmail.com>
2017-03-19 23:05:04 +0530

    task was udpated

--- src/goals/tasks.py
+++ src/goals/tasks.py
@@ -78,7 +78,7 @@ def send_update_email(id):
         logger.info("Email was not sent to [%s]" % goal.user.email)
 
 
-@periodic_task(run_every=crontab(hour="9", minute="0"))
+@periodic_task(run_every=crontab(hour="23", minute="07"))
 def daily_notification():
     subject = "Mysana Daily Notification"
     template = loader.get_template("goals/email/daily_notification.html")
@@ -86,24 +86,26 @@ def daily_notification():
     users = User.objects.all()
     for user in users:
         goals = user.goal_set.all()
-        html_message = template.render({
-            "user": user.name.title(),
-            "goals": goals,
-            "server": os.environ.get('SERVER_NAME', 'mysana.rohanroy.com')
-        })
-        logger.info("sending daily notifications for user [%s]" % user.id)
-        send_mail(
-            subject,
-            '',
-            settings.EMAIL_FROM,
-            [user.email],
-            html_message=html_message,
-            fail_silently=False
-        )
-        logger.info("Daily notifications was sent to [%s]" % user.email)
+        logger.info("Goals count for user %s is %s" % (user.id, goals.count()))
+        if goals.count() > 1:
+            html_message = template.render({
+                "user": user.name.title(),
+                "goals": goals,
+                "server": os.environ.get('SERVER_NAME', 'mysana.rohanroy.com')
+            })
+            logger.info("sending daily notifications for user [%s]" % user.id)
+            send_mail(
+                subject,
+                '',
+                settings.EMAIL_FROM,
+                [user.email],
+                html_message=html_message,
+                fail_silently=False
+            )
+            logger.info("Daily notifications was sent to [%s]" % user.email)
 
 
-@periodic_task(run_every=crontab(hour="22", minute="40"))
+@periodic_task(run_every=crontab(hour="23", minute="07"))
 def overdue_task():
     template = loader.get_template("goals/email/overdue_notification.html")
     now = datetime.now()
@@ -111,6 +113,7 @@ def overdue_task():
     users = User.objects.all()
     for user in users:
         goals = user.goal_set.all().filter(end_date__gte=now)
+        logger.info("Goals count for user %s is %s" % (user.id, goals.count()))
         if goals.count() > 0:
             html_message = template.render({
                 "user": user.name.title(),
